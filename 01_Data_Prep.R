0443e3d34add0860df167a020f3375855e00b824
DESKTOP-K9RMO74\clewi <lewi0655@umn.edu>
2018-07-23 22:32:12 -0500

    split data by off, def, special teams

--- 01_Data_Prep.R
+++ 01_Data_Prep.R
@@ -12,19 +12,41 @@ pacman::p_load(tidyverse, rvest)
 example_page <- read_html("http://www.hostedstatistics.com/football/team_stats.asp?league=WFA&season=2018&tier=WFA+I&selected_team=Titans&selected_week=1")
 
 # use this to name the tables: 
-tab.names <-  example_page %>% 
+tbl.names <-  example_page %>% 
   html_nodes(".indStatHead") %>%
-  html_text()
+  html_text() %>%
+  trimws() 
 
-# Read in the division 2 statistics
-d2.dta.list <- readRDS("div_2_stats.rds")
+# Read in the statistics for all division 
+dta.list <- readRDS("Data/div_all_stats.rds")
 
 # name the tables using tab
-names(d2.dta.list) <- tab.names
+names(dta.list) <- tbl.names
 
-# combine data
-full.dta <- d2.dta %>% 
-  reduce(full_join, by = c("Name", "No.", "team", "week")) %>%
+# group names into Offense, Defense and special teams: 
+off.tbl.names <- c("Passing", "Rushing", "Receiving")
+def.tbl.names <- c("Tackles", "Sacks", "Forced Fumbles"
+                   , "Interceptions (Top 25)", "Fumble Recoveries"
+                   , "Pass Deflections")
+sp.tbl.names <- c("Blocked Kicks & Punts", "Kickoff Returns"
+                  , "Punt Returns", "Punting", "Touchdowns"
+                  ,"Field Goals", "PATs", "2-Point PATs", "Safeties")
+
+
+
+# Bind the listed datasets for offense, defense, and special teams, separately. 
+off.dta <- dta.list[off.tbl.names] %>% 
+  reduce(full_join, by = c("Name", "No.", "team", "week", "season")) %>%
   select(-contains("Rnk"))
 
-View(full.dta)
+def.dta <- dta.list[def.tbl.names] %>% 
+  reduce(full_join, by = c("Name", "No.", "team", "week", "season"), suffix) %>%
+  select(-contains("Rnk"))
+  
+
+sp.dta <- dta.list[sp.tbl.names] %>% 
+  reduce(full_join, by = c("Name", "No.", "team", "week", "season")) %>%
+  select(-contains("Rnk"))
+
+
+
